74e2240e40a9bf788210056cbdf77a3d4d9e3043
Naoufal Kadhom <naoufalkadhom@gmail.com>
2014-08-17 19:23:15 -0400

    add xcode command line tools install script

---
+++ xcode/install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# Xcode Command Line Tools
+#
+# Installs Xcode Command Line Tools
+
+
+
+# Install Xcode Command Line Tools
+echo “Installing Xcode Command Line Tools”
+xcode-select --install
+
